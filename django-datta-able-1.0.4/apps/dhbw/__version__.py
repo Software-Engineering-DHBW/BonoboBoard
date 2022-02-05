ebe0f6a1b996dc790d9b3dd4eb825f26cf945f8c
Na1k <nick.kramer9926@gmail.com>
2022-02-05 11:04:11 +0100

    Requirement Analysis commit for tag

--- django-datta-able-1.0.4/apps/dhbw/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = "dhbw"
-__description__ = "DHBW Scrape Utility"
-__url__ = "https://github.com/Software-Engineering-DHBW/BonoboBoard/blob/main/README.md"
-__version__ = "0.0.1"
-__author__ = ["", "", "", "", "Jakob Hutschenreiter"]
-__author_email__ = ["", "", "", "", "jakob.hutschenreiter@gmx.net"]
-__license__ = ""
