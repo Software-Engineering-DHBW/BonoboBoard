64786d4fa82d1c63cb4ccc2473cd669e650b8b11
Patrick Küsters <kuesters.patrick@gmail.com>
2022-02-20 16:03:48 +0100

    updates sidebar navigation and added destination htmls

--- bonobo-board/apps/home/urls.py
+++ bonobo-board/apps/home/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    path('leistungsuebersicht', views.leistungsuebersicht, name='leistungsuebersicht'),
+    path('email', views.email, name='email'),
+    path('vorlesungsplan', views.vorlesungsplan, name='vorlesungsplan'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
