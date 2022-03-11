75d15de7f93258f4a49e9aa1df8300b4bad9a238
Patrick Küsters <kuesters.patrick@gmail.com>
2022-03-11 12:20:43 +0100

    ref authenticate_user

--- bonobo-board/apps/authentication/views.py
+++ bonobo-board/apps/authentication/views.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, get_user_model
+from django.contrib.auth import login, get_user_model, authenticate
 
 from .forms import LoginForm
 from .models import BonoboUser
@@ -16,23 +16,6 @@ from dhbw.moodle import MoodleImporter
 from dhbw.zimbra import ZimbraHandler
 
 BonoboUser = get_user_model()
-
-def authenticate_user(username, password, course):
-    # TODO
-    bonobo_user = BonoboUser.objects.create_user(username)
-    course_uid = CourseImporter().get_course_uid(course)
-    bonobo_user.user_objects["dualis"] = DualisImporter()
-    bonobo_user.user_objects["lecture"] = LectureImporter(course_uid)
-    bonobo_user.user_objects["moodle"] = MoodleImporter()
-    bonobo_user.user_objects["zimbra"] = ZimbraHandler()
-    
-    for elem in ["dualis", "moodle", "zimbra"]:
-        bonobo_user.user_objects[elem].login(username, password)
-        if not bonobo_user.user_objects[elem].auth_token:
-            return None
-        bonobo_user.user_objects[elem].scrape()
-
-    return bonobo_user
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -47,7 +30,7 @@ def login_view(request):
             course = form.cleaned_data.get("course")
             # TODO load stored user data from db
             # write own function therefore!
-            user = authenticate_user(username, password, course)
+            user = authenticate_user(request, username, password, course)
             if user is not None:
                 login(request, user)
                 return redirect("/")
@@ -57,3 +40,34 @@ def login_view(request):
             msg = 'Error validating the form'
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
+
+def authenticate_user(request, username, password, course):
+
+    dualis= DualisImporter()
+    moodle = MoodleImporter()
+    zimbra = ZimbraHandler()
+
+    for element in [dualis, moodle, zimbra]:
+        element.login(username, password)
+        #if dualis present -> login
+        if not element.auth_token:
+            return None
+        element.scrape()
+
+    if BonoboUser.objects.filter(email=username).exists():
+        bonobo_user = authenticate(request, email=username, password=username)
+    if bonobo_user is None:
+        return None
+    else:
+        bonobo_user = BonoboUser.objects.create_user(email=username, password=username)
+    
+    course_uid = CourseImporter().get_course_uid(course)
+    lecture = LectureImporter(course_uid)
+
+    bonobo_user.user_objects["dualis"] = dualis
+    bonobo_user.user_objects["lecture"] = lecture
+    bonobo_user.user_objects["moodle"] = moodle
+    bonobo_user.user_objects["zimbra"] = zimbra
+
+    return bonobo_user
