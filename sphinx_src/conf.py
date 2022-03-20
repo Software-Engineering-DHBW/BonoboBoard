35c535a8e32de90c37a1289f16944959f1c159a0
Na1k <nick.kramer9926@gmail.com>
2022-03-20 17:18:32 +0100

    ref

--- sphinx_src/conf.py
+++ sphinx_src/conf.py
@@ -16,10 +16,12 @@ _HERE = os.path.dirname(__file__)
 _ROOT_DIR = os.path.abspath(os.path.join(_HERE, '..'))
 _PACKAGE_DIR = os.path.abspath(os.path.join(_HERE, '../bonobo-board'))
 _MODULES_DIR = os.path.abspath(os.path.join(_HERE, '../bonobo-board/modules'))
+_APPS_DIR = os.path.abspath(os.path.join(_HERE, '../bonobo-board/apps'))
 
 sys.path.insert(0, _ROOT_DIR)
 sys.path.insert(0, _PACKAGE_DIR)
 sys.path.insert(0, _MODULES_DIR)
+sys.path.insert(0, _APPS_DIR)
 
 
 # sys.path.insert(0, os.path.abspath('../bonobo-board'))
@@ -48,7 +50,7 @@ extensions = [
 ]
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
-autodoc_mock_imports = ["django", "icalendar", "core.settings", "sqlalchemy", "datetime", "decouple", "unipath", "argparse", "unittest", "dhbw.tests"]
+autodoc_mock_imports = ["django", "forms", "icalendar", "core.settings", "sqlalchemy", "datetime", "decouple", "unipath", "argparse", "unittest", "dhbw.tests"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
