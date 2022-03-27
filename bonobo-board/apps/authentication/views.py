fec8037107a79762ed025f4e999df480cbe12871
Na1k <nick.kramer9926@gmail.com>
2022-03-27 20:03:58 +0200

    incomplete authentication/views.py, pushed for testing

--- bonobo-board/apps/authentication/views.py
+++ bonobo-board/apps/authentication/views.py
@@ -17,7 +17,10 @@ from dhbw.lecture_importer import CourseImporter, LectureImporter
 from dhbw.moodle import MoodleImporter
 from dhbw.zimbra import ZimbraHandler
 
+from dhbw.lecture_importer import LectureImporter, read_lectures_from_database
+
 BonoboUser = get_user_model()
+
 
 def login_view(request):
     """on login view is opened, show window login.html
@@ -36,7 +39,8 @@ def login_view(request):
         return redirect("/logout/")
 
     form = LoginForm(request.POST or None)
-    course_list = CourseImporter().course_list
+    course_importer = CourseImporter()
+    course_list = course_importer.course_list
     msg = None
     if request.method == "POST":
 
@@ -48,8 +52,8 @@ def login_view(request):
             if not is_valid_course(course_list, course):
                 msg = 'Unbekannter Kurs'
                 return render(request, "accounts/login.html", {"form": form, "msg": msg, "course_list": course_list})
-
-            user, err_msg = authenticate_user(request, username, password, course)
+            lectures_json = lecture_handler(course_importer, course)
+            user, err_msg = authenticate_user(request, username, password, course, lectures_json)
 
             if user is not None:
                 login(request, user)
@@ -61,14 +65,27 @@ def login_view(request):
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg, "course_list": course_list})
 
+def lecture_handler(course_importer, course):
+    write_log("lecture_handler")
+    try:
+        lecture_df = read_lectures_from_database(course_importer.get_course_uid(course))
+    except Exception:
+        write_log("in exception")
+        lecture_importer = LectureImporter()
+        asyncio.run(lecture_importer.scrape(course_importer.get_course_uid(course)))
+        lecture_df = lecture_importer.lectures
 
-def authenticate_user(request, username, password, course):
+    return lecture_df.to_json()
+
+def authenticate_user(request, username, password, course, lectures_json):
     """on login view is opened, show window login.html
     authenticate user
     scrape data for user
 
     Parameters
     ----------
+    lectures_json : json
+        return of lecture_handler (gathers lectures from db)
     request: HttpRequest
         request of the page
     username: str
@@ -82,10 +99,10 @@ def authenticate_user(request, username, password, course):
     HttpResponse
     """
     loop = get_new_event_loop()
-    err_msg, dualis_result, moodle_result, zimbra_result, lecture_result = loop.run_until_complete(
+    err_msg, dualis_result, moodle_result, zimbra_result = loop.run_until_complete(
         verify_login_credentials(username, password))
     loop.close()
-    #if user can't be logged in into one or more dhbw services, don't allow login into bonoboboard
+    # if user can't be logged in into one or more dhbw services, don't allow login into bonoboboard
     if err_msg:
         return None, err_msg
 
@@ -100,11 +117,10 @@ def authenticate_user(request, username, password, course):
     bonobo_user.user_objects["dualis"] = dualis_result
     bonobo_user.user_objects["moodle"] = moodle_result
     bonobo_user.user_objects["zimbra"] = zimbra_result
-    bonobo_user.user_objects["lecture"] = lecture_result
 
     asyncio.run(load_user_data(bonobo_user, course))
 
-    #save scraped data to user in db
+    # save scraped data to user in db
     bonobo_user.dualis_scraped_data = bonobo_user.user_objects["dualis"].scraped_data
 
     bonobo_user.zimbra_token = bonobo_user.user_objects["zimbra"].auth_token
@@ -116,7 +132,8 @@ def authenticate_user(request, username, password, course):
     bonobo_user.moodle_token = bonobo_user.user_objects["moodle"].auth_token
     bonobo_user.moodle_scraped_data = bonobo_user.user_objects["moodle"].scraped_data
 
-    bonobo_user.lectures = bonobo_user.user_objects["lecture"].lectures.to_json()
+    bonobo_user.lectures = lectures_json
+
     bonobo_user.save()
     return bonobo_user, ""
 
@@ -167,20 +184,18 @@ async def verify_login_credentials(username, password):
         MoodleImporter().login(username, password))
     zimbra_result = asyncio.ensure_future(
         ZimbraHandler().login(username, password))
-    lecture_result = asyncio.ensure_future(
-        LectureImporter().login())
 
     error_msg = ""
 
     try:
-        await asyncio.gather(dualis_result, moodle_result, zimbra_result, lecture_result)
+        await asyncio.gather(dualis_result, moodle_result, zimbra_result)
     except ServiceUnavailableException as service_err:
         error_msg = f"{service_err}"
-        return error_msg, None, None, None, None
+        return error_msg, None, None, None
     except CredentialsException as cred_err:
         error_msg = f"{cred_err}"
-        return error_msg, None, None, None, None
-    return error_msg, dualis_result.result(), moodle_result.result(), zimbra_result.result(), lecture_result.result()
+        return error_msg, None, None, None
+    return error_msg, dualis_result.result(), moodle_result.result(), zimbra_result.result()
 
 
 async def load_user_data(bonobo_user, course):
@@ -202,12 +217,9 @@ async def load_user_data(bonobo_user, course):
         bonobo_user.user_objects["moodle"].scrape())
     zimbra_future = asyncio.ensure_future(
         bonobo_user.user_objects["zimbra"].scrape())
-    course_uid = CourseImporter().get_course_uid(course)
-    lecture_future = asyncio.ensure_future(
-        bonobo_user.user_objects["lecture"].scrape(course_uid))
 
     try:
-        await asyncio.gather(dualis_future, moodle_future, zimbra_future, lecture_future)
+        await asyncio.gather(dualis_future, moodle_future, zimbra_future)
     except LoginRequiredException:
         # TODO something meaningful x)
         pass
