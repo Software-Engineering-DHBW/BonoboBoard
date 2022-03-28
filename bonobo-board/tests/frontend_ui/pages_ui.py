1bc42b4ba8485d99e9c6bb39080b74ecf5388f04
Jakob Hutschenreiter <jakob.hutschenreiter@gmx.net>
2022-03-28 16:46:31 +0200

    ui test (feature: leistungsuebersicht) works now for chrome, firefox, brave

--- bonobo-board/tests/frontend_ui/pages_ui.py
+++ bonobo-board/tests/frontend_ui/pages_ui.py
@@ -10,6 +10,7 @@ from unittest import TestCase
 from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -47,14 +48,18 @@ def add_browsers_to_test():
     for elem in drivers:
         if elem == "chrome":
             options = webdriver.ChromeOptions()
+            options.add_experimental_option("detach", True)
+            options.add_argument("--start-maximized")
             browsers.append(
                 webdriver.Chrome(
                     service=ChromeService(ChromeDriverManager().install()),
-                    options=options.add_experimental_option("detach", True)
+                    options=options
                 )
             )
         elif elem == "brave":
             options = webdriver.ChromeOptions()
+            options.add_experimental_option("detach", True)
+            options.add_argument("--start-maximized")
             browsers.append(
                 webdriver.Chrome(
                     service=ChromeService(
@@ -62,7 +67,7 @@ def add_browsers_to_test():
                             chrome_type=ChromeType.BRAVE
                         ).install()
                     ),
-                    options=options.add_experimental_option("detach", True)
+                    options=options
                 )
             )
         elif elem == "firefox":
@@ -96,39 +101,26 @@ class TestPagesUI(TestCase):
             # fill login form
             fill_input_form(browser)
             # locate login btn and click it
-            browser.find_element(by=By.ID, value="login").click()
-            wait = WebDriverWait(browser, 10)
-            wait.until(lambda browser: browser.current_url != login_url)
-            browser.find_element(by=By.ID, value="icon").click()
+            login_btn = browser.find_element(by=By.ID, value="login")
+            login_btn.click()
+            wait = WebDriverWait(browser,10)
+            wait.until(EC.url_changes(fconf.URL))
+            #wait = WebDriverWait(browser, 10)
+            #wait.until(lambda browser: browser.current_url != login_url)
+            browser.implicitly_wait(3)
+            icon_btn = browser.find_element(by=By.ID, value="icon")
+            icon_btn.click()
             gpa_div = browser.find_element(by=By.ID, value="gpa")
             gpa_div_p = gpa_div.find_element(by=By.TAG_NAME, value="p")
             self.assertRegex(gpa_div_p.text, r"GPA-Total:\x20[0-6]\.[0-9]$")
-            browser.find_element(by=By.LINK_TEXT, value="Leistungsübersicht").click()
+            browser.implicitly_wait(3)
+            navbar_grades = browser.find_element(by=By.LINK_TEXT, value="Leistungsübersicht")
+            navbar_grades.click()
             self.assertIn("/leistungsuebersicht", browser.current_url)
             all_grades_div = browser.find_element(by=By.ID, value="all_grades")
             tbody = all_grades_div.find_element(by=By.TAG_NAME, value="tbody")
             grades = tbody.text.split("\n")
             self.assertRegex(grades[0], r".+\x20[0-9]+\x20[0-6]\.[0-9]$")
-
-    # def test_authentication(self):
-    #     """test UI authentication for the browser"""
-    #     check_env_vars_set()
-    #     for browser in self.browsers:
-    #         browser.get(fconf.URL)
-    #         self.assertEqual("BonoboBoard - Login", browser.title)
-    #         # fill login form
-    #         fill_input_form(browser)
-    #         # locate login btn and click it
-    #         browser.find_element(by=By.ID, value="login").click()
-
-
-    # def test_leistungsuebersicht_home(self):
-    #     """assert that the gpa total grade is set on the homepage"""
-    #     for browser in self.browsers:
-    #         browser.get(fconf.URL)
-    #         gpa_div = browser.find_element(by=By.ID, value="gpa")
-    #         gpa_div_p = gpa_div.find_element(by=By.TAG_NAME, value="p")
-    #         self.assertRegex(gpa_div_p.text, r"GPA-Total:\x20[1-6]\.[0-9]")
 
     def tearDown(self):
         """quit all used browsers"""
@@ -139,5 +131,5 @@ class TestPagesUI(TestCase):
 # see here for details: https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/5923
 if __name__ == "__main__":
     global drivers
-    drivers = ["firefox", ]
+    drivers = ["chrome", "firefox", "brave"]
     unittest.main(verbosity=2)
